d15098672b493322811c028e31f8485e1bfea325
KOSASIH <kosasihg88@gmail.com>
2024-12-16 19:30:40 +0700

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# Package initialization for the src module
+
+# You can include any package-level variables or imports here
+__all__ = [
+    "main",
+    "alerting",
+    "log_management",
+    "logging"
+]
